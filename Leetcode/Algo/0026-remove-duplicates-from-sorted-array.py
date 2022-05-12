b51a5fe581c75c4f7ed5919a1aec583cd5c07374
Tri Nguyen <nguyenminhtricpr@gmail.com>
2022-05-12 20:17:44 +0700

    Leetcode 26

---
+++ Leetcode/Algo/0026-remove-duplicates-from-sorted-array.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i = 0
+        j = 0
+        while j < len(nums):
+            if nums[j] != nums[i]:
+                i+=1
+                nums[i] = nums[j]
+                j+=1
+            else:
+                j+=1
+        return i + 1
